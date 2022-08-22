ba0357928394cb406316654a4d9ff3b9d1f03450
jeremias <jeremias.vallejos@gmail.com>
2022-08-22 13:22:31 -0300

    ultimo commit

--- client/src/Components/FormTasks/index.js
+++ client/src/Components/FormTasks/index.js
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import { Form, Formik } from 'formik'
 import swal from 'sweetalert'
+import '../FormTasks/FormTasks.css'
 
 export default function CreateTasks() {
   const taks = useSelector((state) => state.getTask)
@@ -16,7 +17,7 @@ export default function CreateTasks() {
         title:'',
         description:''
   })
-  
+  console.log(task.title)
   useEffect(()=>{
     const loadTasks = async()=>{
   if(params.id){
@@ -31,61 +32,77 @@ export default function CreateTasks() {
   }, [])
 
   return (
-    <div>
-      <h1>{params.id ? 'Edit tasks': 'Create tasks'}</h1>
+      <>
+      <h1 className='tituloh1'>{params.id ? 'Edit tasks': 'Create tasks'}</h1>
+      <div className='contenedor-from'>
       <Formik
         initialValues={task}
         enableReinitialize={true}
         onSubmit={(value, action) => {
-          if(params.id){
+          if(params.id && value.title && value.description){
           dispatch(updateTasks(params.id, value))
           swal({
-            title: "Good job!",
-            text: "tu tarea fue madificada!",
+            title: "Buen tarabajo!",
+            text: "Tu tarea fue madificada!",
             icon: "success",
           });
-            action.resetForm()// reseta el input cunado se termina de inviar la info NO ANDA
-          }else{
-            console.log(value)
-          dispatch(createTasks(value))
-          swal({
-            title: "Good job!",
-            text: "tu tarea fue creada!",
-            icon: "success",
-          });
-           action.resetForm()// reseta el input cunado se termina de inviar la info NO ANDA
-          }
-
           navigate("/tasks")
-        }}
+          } else if(params.id && !value.title && !value.description){
+            swal({
+              title: "Error!",
+              text: "Tu tarea no pudo ser modificada por falta de informacion!",
+              icon: "warning",
+          })
+          navigate("/tasks")
+        }else if(value.title && value.description){
+            console.log(value.title)
+            dispatch(createTasks(value))
+           swal({
+            title: "Buen trabajo!",
+            text: "Tu tarea fue creada!",
+            icon: "success",
+          });
+          navigate("/tasks")
+          }else{
+            swal({
+              title: "Error!",
+              text: "Tu tarea no pudo ser creada por falta de informacion!",
+              icon: "warning",
+          })
+          navigate("/tasks")
+      }
+    }}
       >
 
-        {({ handleChange, handleSubmit, values, isSubmitting}) => (
+        {({ handleChange, handleSubmit, values}) => (
           <Form onSubmit={handleSubmit}>
-            <label>Title</label>
+            <h1 className='label-title'>Title:</h1>
             <input
               placeholder='ingrese un titulo'
+              className='input-title'
               type={'text'}
               name='title'
               onChange={handleChange}
               value={values.title} // es para limpiar el input NO ANDA
             /><br/>
-            <label>Description</label>
+            <h1 className='label-description'>Description:</h1>
             <textarea
               type={'text'}
+              className='input-descripction'
               name='description'
               rows={'3'}
               placeholder='ingrese una descripcion de la tarea'
               onChange={handleChange}
               value={values.description}
             ></textarea><br/>
-            <button type={'submit'} disabled={isSubmitting} >
-              {isSubmitting ? 'Saving...': 'Save'}
-              </button><br/>
-             <Link to={'/tasks'}><button>Volver</button></Link>
+            <div className='contenedor-botoness'>
+            <button className='boton-save' type={'submit'} >Save</button><br/>
+             <Link to={'/tasks'} className='LINKK'><button className='boton-volver'>Volver</button></Link>
+             </div>
           </Form>
         )}
       </Formik>
     </div>
+    </>
   )
 }
